fece572ca0e5bf5faf379cc8ac63d5846c91f737
Nayan0406git config --global user.email <nayannikhare83@gmail.com>
2025-09-03 18:11:07 +0530

    changes

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -11,7 +11,7 @@ const ProductList = () => {
           <div className="overflow-x-auto hide-scrollbar py-4">
             <div className="flex gap-6 items-stretch snap-x snap-mandatory">
               {/* Card 1 */}
-              <article className="snap-center flex-shrink-0 w-56 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+              <article className="snap-center flex-shrink-0 w-68 sm:w-74 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                 <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/irkal-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                 <div className="p-4 flex flex-col flex-1">
                   <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -28,7 +28,7 @@ const ProductList = () => {
               </article>
 
               {/* Card 2 */}
-              <article className="snap-center flex-shrink-0 w-56 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+              <article className="snap-center flex-shrink-0 w-68 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                 <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/tussar-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                 <div className="p-4 flex flex-col flex-1">
                   <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -45,7 +45,7 @@ const ProductList = () => {
               </article>
 
               {/* Card 3 */}
-              <article className="snap-center flex-shrink-0 w-56 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+              <article className="snap-center flex-shrink-0 w-68 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                 <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/solapur-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                 <div className="p-4 flex flex-col flex-1">
                   <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -62,7 +62,7 @@ const ProductList = () => {
               </article>
 
               {/* Card 4 */}
-              <article className="snap-center flex-shrink-0 w-56 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+              <article className="snap-center flex-shrink-0 w-68 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                 <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/Kanjeevaram-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                 <div className="p-4 flex flex-col flex-1">
                   <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -79,7 +79,7 @@ const ProductList = () => {
               </article>
 
               {/* Card 5 */}
-              <article className="snap-center flex-shrink-0 w-56 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+              <article className="snap-center flex-shrink-0 w-68 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                 <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/home-product-img.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                 <div className="p-4 flex flex-col flex-1">
                   <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
--- client/src/components/RecentlyProduct.jsx
+++ client/src/components/RecentlyProduct.jsx
@@ -6,12 +6,11 @@ const RecentlyProduct = () => {
     <section className="py-12">
           <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-3xl lg:text-4xl font-bold mb-6">Recently Viewed</h2>
-    
-            
+
               <div className="overflow-x-auto hide-scrollbar py-4">
                 <div className="flex gap-6 items-stretch snap-x snap-mandatory">
                   {/* Card 1 */}
-                  <article className="snap-center flex-shrink-0 w-64 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+                  <article className="snap-center flex-shrink-0 w-68 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                     <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/irkal-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                     <div className="p-4 flex flex-col flex-1">
                       <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -28,7 +27,7 @@ const RecentlyProduct = () => {
                   </article>
     
                   {/* Card 2 */}
-                  <article className="snap-center flex-shrink-0 w-64 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+                  <article className="snap-center flex-shrink-0 w-68 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                     <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/tussar-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                     <div className="p-4 flex flex-col flex-1">
                       <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -45,7 +44,7 @@ const RecentlyProduct = () => {
                   </article>
     
                   {/* Card 3 */}
-                  <article className="snap-center flex-shrink-0 w-64 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+                  <article className="snap-center flex-shrink-0 w-68 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                     <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/solapur-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                     <div className="p-4 flex flex-col flex-1">
                       <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -62,7 +61,7 @@ const RecentlyProduct = () => {
                   </article>
     
                   {/* Card 4 */}
-                  <article className="snap-center flex-shrink-0 w-64 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+                  <article className="snap-center flex-shrink-0 w-68 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                     <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/Kanjeevaram-saree.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                     <div className="p-4 flex flex-col flex-1">
                       <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">
@@ -79,7 +78,7 @@ const RecentlyProduct = () => {
                   </article>
     
                   {/* Card 5 */}
-                  <article className="snap-center flex-shrink-0 w-64 sm:w-72 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
+                  <article className="snap-center flex-shrink-0 w-68 sm:w-64 md:w-70 bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
                     <div className="h-44 md:h-60 lg:h-75 bg-cover bg-center" style={{backgroundImage: "url('/home-product-img.png')", backgroundPosition: 'center 1%', backgroundSize: 'cover'}} />
                     <div className="p-4 flex flex-col flex-1">
                       <div className="flex items-center justify-between text-xs text-[#a31f1f] mb-2">

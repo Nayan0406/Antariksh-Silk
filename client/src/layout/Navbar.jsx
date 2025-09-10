fa364c2017a26701668673ec38c0ddc999d82879
Nayan0406git config --global user.email <nayannikhare83@gmail.com>
2025-09-10 13:34:10 +0530

    navbar add to cart link added

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
         {/* Icons (desktop) */}
         <div className='hidden md:flex items-center gap-2 lg:gap-3 xl:gap-4 lg:mr-2 xl:mr-3'>
           <button aria-label="Wishlist" className="text-gray-700 cursor-pointer"><CiHeart className='w-6 h-6 lg:w-8 lg:h-8' /></button>
-          <button aria-label="Cart" className="text-gray-700 cursor-pointer"><PiShoppingCartThin className='w-6 h-6 lg:w-8 lg:h-8' /></button>
+          <Link to="/addtocart"><button aria-label="Cart" className="text-gray-700 cursor-pointer"><PiShoppingCartThin className='w-6 h-6 lg:w-8 lg:h-8' /></button></Link>
         </div>
         {/* Auth Buttons (desktop) */}
         <div className="hidden md:flex items-center gap-2 lg:gap-3 xl:gap-4 lg:mr-4 xl:mr-12">
@@ -63,7 +63,7 @@ const Navbar = () => {
             <Link to="/contact" onClick={() => setMenuOpen(false)} className="text-base font-medium text-gray-800 hover:text-[#550000] transition">Contact Us</Link>
             <div className='flex gap-4 mt-2'>
               <button aria-label="Wishlist" className='text-gray-700'><CiHeart className='w-6 h-6' /></button>
-              <button aria-label="Cart" className='text-gray-700'><PiShoppingCartThin className='w-6 h-6' /></button>
+              <Link to="/addtocart"><button aria-label="Cart" className='text-gray-700'><PiShoppingCartThin className='w-6 h-6' /></button></Link>
             </div>
             <div className="flex flex-col gap-2 mt-2">
               <Link to="/login" onClick={() => setMenuOpen(false)}><button className="w-full px-4 py-2 rounded-full border-2 border-[#550000] text-[#b76e79] font-medium bg-white hover:bg-[#f8e6ea] transition cursor-pointer">Login</button></Link>
